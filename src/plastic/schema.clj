9382f403f5d70fe033d137e5b01a63e8308a6c6b
anatoly <tolitius@gmail.com>
2025-02-20 21:17:35 -0500

    add to-bash-map

--- src/plastic/schema.clj
+++ src/plastic/schema.clj
@@ -23,8 +23,30 @@
                              children)]
     (cons current-table child-tables)))
 
-(defn to-tables [jschema]
+(defn to-tables
+  "convert nested json schema to a one level edn map"
+  [jschema]
   (let [schema (json/read-value jschema
                                 default-mapper)]
     {:index  (-> schema first :index (s/replace "%s" ""))
      :tables (-> schema first :nodes flatten-tables vec)}))
+
+(defn to-bash-map
+  "convert an edn schema map to a bash map:
+
+   root_table_name=\"quantum_tunnel\"
+   declare -A schema_fields
+   schema_fields[\"quantum_tunnel\"]='[\"id\", \"quantum_code\", \"quantity\", \"particle_number\", \"created_date\", \"last_modified_date\"]'
+   schema_fields[\"quantum_code\"]='[\"category\", \"milestones\", \"high_point\"]'
+   schema_fields[\"quantum_zone\"]='[\"id\", \"name\"]'
+  "
+  [{:keys [tables]}]
+  (->> tables
+       (group-by :name)
+       (map (fn [[name tables]]
+              [name (distinct (mapcat :fields tables))]))
+       (map (fn [[name fields]]
+              (str "schema_fields[\"" name "\"]='[\"" (s/join "\", \"" fields) "\"]'")))
+       (into [(str "root_table_name=\"" (:name (first tables)) "\"")
+              "declare -A schema_fields"])
+       (s/join "\n")))
